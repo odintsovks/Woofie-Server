3851fb04bc6cd7a82701e31613e01640dbb533a3
odintsovks <ksodintsov03@gmail.com>
2025-12-02 17:58:02 +0300

    flake: add gradle hash

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   in
   {
     devShell.${system} = pkgs.mkShellNoCC {
-      shellHook = ''export JDTLS_CONFIG='cmd = {"${pkgs.jdt-language-server}/bin/jdtls"}' '';
+      shellHook = ''export JDTLS_CONFIG='cmd = {"${pkgs.jdt-language-server}/bin/jdtls"}, init_options = { settings = {java = {imports = {gradle = {enabled = true, wrapper = {enabled = true, checksums = {{sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172", allowed = true}}}}}}}}' '';
       packages = with pkgs; [
         javaPackages.compiler.temurin-bin.jdk-21
         gradle
